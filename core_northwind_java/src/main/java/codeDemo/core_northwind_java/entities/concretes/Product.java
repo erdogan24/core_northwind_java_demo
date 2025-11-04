f6b1cb14800455bfb831379ac988a617f2415eb8
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-11-05 01:39:15 +0300

    Product class has been added in Entity package

---
+++ core_northwind_java/src/main/java/codeDemo/core_northwind_java/entities/concretes/Product.java
@@ -0,0 +1,11 @@
+package codeDemo.core_northwind_java.entities.concretes;
+
+public class Product {
+	private int id;
+	private int categoryId;
+	private String productName;
+	private double unitPrice;
+	private short unitsInStock;
+	private String quantityPerUnit;
+
+}
