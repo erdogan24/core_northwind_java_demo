298c59106b4dbf1a13721dd215595872a1e80848
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-11-04 22:07:34 +0300

    First commit

---
+++ core_northwind_java/src/main/java/codeDemo/core_northwind_java/CoreNorthwindJavaApplication.java
@@ -0,0 +1,13 @@
+package codeDemo.core_northwind_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreNorthwindJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreNorthwindJavaApplication.class, args);
+	}
+
+}
---
+++ core_northwind_java/src/test/java/codeDemo/core_northwind_java/CoreNorthwindJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package codeDemo.core_northwind_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreNorthwindJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
