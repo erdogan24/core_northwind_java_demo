aa2c3ec5717534926ae0afc53056f2fd4aa90842
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-11-07 19:46:25 +0300

    The core layer has been updated.

---
+++ core_northwind_java/src/main/java/codeDemo/core_northwind_java/core/utilities/results/DataResult.java
@@ -0,0 +1,12 @@
+package codeDemo.core_northwind_java.core.utilities.results;
+
+public class DataResult extends Result{
+
+	public DataResult(boolean success, String message) {
+		super(success, message);
+		
+	}
+
+	
+
+}
