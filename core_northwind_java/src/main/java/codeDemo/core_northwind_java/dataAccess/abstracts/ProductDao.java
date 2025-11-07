4438fd3242905990e05f6dc0e6dcd96b23d92b96
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-11-08 01:48:16 +0300

    productService and productDao has been updated.

--- core_northwind_java/src/main/java/codeDemo/core_northwind_java/dataAccess/abstracts/ProductDao.java
+++ core_northwind_java/src/main/java/codeDemo/core_northwind_java/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,25 @@
 package codeDemo.core_northwind_java.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import codeDemo.core_northwind_java.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
-
+	Product getByProductName(String productName);
+	
+	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+	
+	List<Product> getByCategoryIdIn(List<Integer> categories);
+	
+	List<Product> getByProductNameContains(String ProductName);
+	
+	List<Product> getByProductNameStartsWith(String ProductName);
+	
+	@Query("From Product where productName=:productName and categoryId=:categoryId")
+	List<Product> getByNameAndCategory(String productName, int categoryId);
 }
